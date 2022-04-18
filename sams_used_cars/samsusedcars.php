9019ab7669c49dadc18dfdbf5bc8e16b45f77003
Manny <the_raging@live.com>
2022-04-18 15:27:16 -0400

    Create samsusedcars.php

---
+++ sams_used_cars/samsusedcars.php
@@ -0,0 +1,27 @@
+﻿
+<div  class="content" background="bg.jpg">
+
+<h2><table style="width: 100%">
+	<tr>
+		<td style="width: 133px">
+		<img alt="Used Cars" height="120" src="./joyofphp/usedcars.jpg" width="184" /></td>
+		<td><h1 class="auto-style1">Welcome to Sam's Used Cars</h1>
+		<p class="auto-style1">The companion web site that goes with the <a href="index.html">Joy 
+		of PHP</a> Book</p></td>
+	</tr>
+</table>
+</h2>
+<hr />
+<p>Welcome to Sam's Used car lot!</p>
+<p><a href="./joyofphp/formEnterCar.htm">Add a Car</a></p>
+<p><a href="./joyofphp/ViewCars.php">View Cars - Basic</a></p>
+<p><a href="./joyofphp/ViewCarsWithStyle.php">View Cars with Style</a> </p>
+<p><a href="./joyofphp/ViewCarsWithStyle2.php">View Cars with Edit Links</a></p>
+<p><a href="./joyofphp/ViewCarsAddImage.php">Add Images to Cars</a></p>
+
+<p>&nbsp;</p>
+
+<p><img alt="Joy of PHP" height="313" src="./joyofphp/smallcover.jpg" width="196" /></p>
+
+</div>
+
